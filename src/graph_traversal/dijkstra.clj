5d0c41add216dc9323965e5173ef12c60c333632
milad <milad.vadoodparast@gmail.com>
2024-10-13 17:08:44 +1100

    update dijkstra to throw exceptions when there's no path

--- src/graph_traversal/dijkstra.clj
+++ src/graph_traversal/dijkstra.clj
@@ -1,14 +1,16 @@
 (ns graph-traversal.dijkstra
   (:require [clojure.data.priority-map :as pmap]))
 
+
 ;; Dijkstra's algorithm to find the shortest path in a weighted graph
 (defn shortest-path
-  "Finds the shortest path from `start` to `end` in a weighted graph `g`."
+  "Finds the shortest path from `start` to `end` in a weighted graph `g`.
+  Throws a regular `Exception` if no path is found."
   [g start end]
   (letfn [(dijkstra [frontier dist prev]
             (if (empty? frontier)
               (if (nil? (dist end))
-                nil ; Return nil if there's no path to `end`
+                (throw (Exception. (str "No path found from " start " to " end))) ; Throw regular exception
                 (reverse (loop [v end path []]
                            (if (nil? v)
                              path
@@ -30,10 +32,3 @@
                      (merge dist (into {} updates))
                      (reduce (fn [p [n _]] (assoc p n v)) prev updates)))))))]
     (dijkstra (pmap/priority-map start 0) {start 0} {})))
-
-;; Define a weighted graph example
-(def weighted-graph
-  {:1 [[:2 1] [:3 2]],
-   :2 [[:4 4]],
-   :3 [[:4 2]],
-   :4 []})
--- test/graph_traversal/dijkstra_test.clj
+++ test/graph_traversal/dijkstra_test.clj
@@ -8,12 +8,17 @@
         :4 []})
 
 (deftest test-shortest-path
-  (testing "Shortest path in a weighted graph"
+  (testing "Find shortest path when it exists"
+    ;; Test valid paths
     (is (= (shortest-path G :1 :4) [:1 :3 :4]))
     (is (= (shortest-path G :2 :4) [:2 :4]))
-    (is (= (shortest-path G :3 :4) [:3 :4]))
-    (is (= (shortest-path G :1 :3) [:1 :3]))
-    (is (nil? (shortest-path G :4 :1)))))
+    (is (= (shortest-path G :1 :3) [:1 :3]))))
 
-;; Run all the tests
+(deftest test-no-path
+  (testing "Throws exception when no path is found"
+    ;; No path from :4 to :1
+    (is (thrown? Exception (shortest-path G :4 :1)))
+    ;; No path from :3 to :2 (as it’s a directed graph)
+    (is (thrown? Exception (shortest-path G :3 :2)))))
+
 (run-tests)
